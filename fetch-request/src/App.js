e228efe9eb0d3730b46a75afe1af28b1f31303f9
Ian Connell <ian10gt@gmail.com>
2022-04-19 22:43:34 +0100

    h1 update

--- fetch-request/src/App.js
+++ fetch-request/src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+const MyFetch = () => {
+    const [facts, setFacts] = useState([])
+    const [error, setError] = useState(null)    
+    
+    useEffect(() => {
+    const fetchApiFacts = async () => {
+        try{
+            const response = await fetch('https://cat-fact.herokuapp.com/facts');
+        if (!response.ok) {
+            throw new Error(response.statusText);
+        }    
+        const data = await response.json();
+        setFacts(data);    
+    } catch (err) {
+        setError('Could not fetch data');
+        console.log(err.message);
+    }
+    }
+
+
+    fetchApiFacts()
+    }, []);
+
+    return (
+        <div>
+          <h1>first branch version</h1>
+            {error && <p>{error}</p>}
+            {facts.map((x) => (
+                <p>{x.text} </p>
+            ))}
+        </div>
+    )
+    }
+
+export default MyFetch
